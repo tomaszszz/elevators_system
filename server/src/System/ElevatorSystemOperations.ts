fff9802beb2834769825cce1de4c78395ddb860a
trozko <trozko@griddynamics.com>
2024-07-16 22:07:00 +0200

    reorganize repo

--- server/src/System/ElevatorSystemOperations.ts
+++ server/src/System/ElevatorSystemOperations.ts
@@ -3,6 +3,7 @@ import { Elevator } from '../Elevator/Elevator';
 
 export interface ElevatorSystemOperations {
     callFromHallway(targetFloor: number, direction: Direction): void;
+    callFromElevator(elevatorId: string, targetFloor: number): void;
     state(): { floorsCount: number; elevators: Elevator[] };
     step(): void;
 }
